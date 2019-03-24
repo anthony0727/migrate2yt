e2a2e297ee18e6fe2905c944a65226f1247bc67f
anthony0727 <jwseok0727@gmail.com>
2019-03-24 14:31:37 +0900

    init

--- app/youtube_util/like_video.py
+++ app/youtube_util/like_video.py
@@ -10,7 +10,6 @@ from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
 
-
 # The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
 # the OAuth 2.0 information for this application, including its client_id and
 # client_secret. You can acquire an OAuth 2.0 client ID and client secret from
@@ -43,43 +42,45 @@ https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account.
-YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
-YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube_util"
+YOUTUBE_API_SERVICE_NAME = "youtube_util"
 YOUTUBE_API_VERSION = "v3"
 
+
 def get_authenticated_service(args):
-  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
-    scope=YOUTUBE_READ_WRITE_SCOPE,
-    message=MISSING_CLIENT_SECRETS_MESSAGE)
+    flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
+                                   scope=YOUTUBE_READ_WRITE_SCOPE,
+                                   message=MISSING_CLIENT_SECRETS_MESSAGE)
 
-  storage = Storage("%s-oauth2.json" % sys.argv[0])
-  credentials = storage.get()
+    storage = Storage("%s-oauth2.json" % sys.argv[0])
+    credentials = storage.get()
 
-  if credentials is None or credentials.invalid:
-    credentials = run_flow(flow, storage, args)
+    if credentials is None or credentials.invalid:
+        credentials = run_flow(flow, storage, args)
 
-  return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    http=credentials.authorize(httplib2.Http()))
+    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                 http=credentials.authorize(httplib2.Http()))
+
 
 # Add the video rating. This code sets the rating to "like," but you could
 # also support an additional option that supports values of "like" and
 # "dislike."
 def like_video(youtube, video_id):
-  youtube.videos().rate(
-    id=video_id,
-    rating="like"
-  ).execute()
+    youtube.videos().rate(
+        id=video_id,
+        rating="like"
+    ).execute()
+
 
 if __name__ == "__main__":
-  argparser.add_argument("--videoid", default="L-oNKK1CrnU",
-    help="ID of video to like.")
-  args = argparser.parse_args()
+    argparser.add_argument("--videoid", default="L-oNKK1CrnU",
+                           help="ID of video to like.")
+    args = argparser.parse_args()
 
-  youtube = get_authenticated_service(args)
-  try:
-    like_video(youtube, args.videoid)
-  except HttpError, e:
-    print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
-  else:
-    print "%s has been liked." % args.videoid
-
+    youtube = get_authenticated_service(args)
+    try:
+        like_video(youtube, args.videoid)
+    except HttpError as e:
+        print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
+    else:
+        print("%s has been liked." % args.videoid)
